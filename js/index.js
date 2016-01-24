e315e08a6d939bbcf708ff20f4152ff7ad4001c2
Alain P <saintaw@gmail.com>
2016-01-25 00:52:48 +0100

    Continents

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 //index.js
-if (typeof localStorage.getItem('countries') === 'undefined' || localStorage.getItem('countries') === null) 
+var $countries = "";
+var $capitals = "";
+var $continents = "";
+var CountryStack = [];
+var CityStack = [];
+var SelectedCountry = "";
+
+/* copy arrays in localstorage if they don't exist */
+if (typeof localStorage.getItem('continents') === 'undefined' || localStorage.getItem('continents') === null) 
 	{
-    // variable is undefined or null
-	var $countries = "";
-	var $capitals = "";
+
 
 	$.getJSON( "./json/names.json", function( data ) {
 		$countries = data;
@@ -17,26 +23,53 @@ if (typeof localStorage.getItem('countries') === 'undefined' || localStorage.get
 		localStorage.setItem('capitals', tmp);		
 	});
 
+	 $.getJSON( "./json/continent.json", function( data ) {
+		$continents  = data;
+		var tmp = JSON.stringify($continents);
+		localStorage.setItem('continents', tmp);		
+	});
+
+
+
 	}
 else
 	{
 		console.log('Storage vars already exist');
-
+	$countries = $.parseJSON(localStorage.countries);
+	$capitals = $.parseJSON(localStorage.capitals);
+	$continents = $.parseJSON(localStorage.continents);		
 	}
-//console.log(localStorage.countries);
-var CountryStack = [];
-var CityStack = [];
+
+
 
 LoadCountries();
 LoadCapitals();
 
-var SelectedCountry = "";
+
 
 
 
 $( "#continents-but" ).click(function() {
 
-	alert('soon');
+	//console.log($continents);
+	$.each( $continents, function( key, value ) {
+	  //console.log( key + ": " + value );
+	  var curr_country = key;
+	  var curr_continent = value;
+
+		$.each( $('.list-capitals'), function( key, value ) {
+		  if ($(this).data('country') == curr_country)
+		  $(this).append('<span class="badge">'+curr_continent +'</span>');
+		});
+
+		$.each( $('.list-countries'), function( key, value ) {
+		  if ($(this).data('country') == curr_country)
+		  $(this).append('<span class="badge">'+curr_continent +'</span>');
+		});
+
+	});
+
+
 
 });
 
@@ -72,8 +105,6 @@ $( ".list-capitals" ).click(function() {
 $( ".list-countries" ).click(function() {
 	var master = $(this).data('country');
 	var master_text = $(this).text();
-	//console.log(SelectedCountry,master);
-
 	if (SelectedCountry == master) {
 		$(this).addClass('green');
 	}
@@ -98,7 +129,7 @@ function AddStory(Country) {
 
 	if (SelectedCountry == Country) {
 		$tmp = "<li class='list-group-item green'>" + $capitals[SelectedCountry] + " -> " + $countries[Country]+ "</li>";
-		//delete the lis in both lists
+		//delete the li in both lists
 		var tmp_country = 'ctry_' +Country;
 		var tmp_capital = 'cap_' +Country;
 		$('#' +tmp_country).delay(300).hide('slow');
@@ -134,41 +165,26 @@ function AddStory(Country) {
 
 
 function LoadCapitals() {
-var $capitals = $.parseJSON(localStorage.capitals);
 $( "#capitals ul" ).remove();
-
-
   var items = [];	
   $.each( $capitals, function( key, val ) {
     if (val !== "")
     items.push( "<li class='list-group-item list-capitals' id='cap_" + key+ "' data-country='" + key + "'>" + val + "</li>" );
   });
-
-
 	shuffle(items);
-
   $( "<ul />", {
     "class": "list-group",
     html: items.join( "" )
   }).appendTo( "#capitals" );
   
 }
-
-
-
 function LoadCountries() {
-var $countries = $.parseJSON(localStorage.countries);
-
-
-
 $( "#countries ul" ).remove();
   var items = [];	
   $.each( $countries, function( key, val ) {
   	var l_key = key.toLowerCase();
     items.push( "<li class='list-group-item list-countries' id='ctry_" + key+ "' data-country='" + key + "'><img src='./media/flags/" + l_key + ".png' /> &nbsp;" + val + "</li>" );
   });
-
-  //shuffle(items);
   items.sort();
 
   $( "<ul />", {
@@ -182,86 +198,9 @@ function shuffle(o){
     for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
 }
-/*
-RenderList($capitals,$countries);
-
-function RenderList(capitals,countries) {
-console.log(capitals)
-}
-*/
-
 
 
 /*
-//set an array locally
-if (typeof localStorage.getItem('ColourData') === 'undefined' || localStorage.getItem('ColourData') === null) 
-	{
-    // variable is undefined or null
-    InitColourStorage();
-
-	}
-else
-	{
-	var strColourSet = localStorage.getItem('ColourData');
-	var arrColourset = JSON.parse(localStorage.getItem('ColourData'));
-	console.log(arrColourset);
-
-
-
-	$.each(arrColourset.colorsArray, function( index, value ) {
-	  //loop elements (I need to add another loop within)
-	  console.log('looop!');
-	  var tmp = arrColourset.colorsArray[index];
-	  console.log(tmp);
-
-	  DebugText(tmp);
-
-
-	});
-
-
-	}
-
-
-function InitColourStorage()
-	{
-    console.log('Does not exist, create it');
-		 Colourset = {
-		    "colorsArray":[{
-		            "colorName":"red",
-		            "hexValue":"#f00"
-		        },
-		        {
-		            "colorName":"green",
-		            "hexValue":"#0f0"
-		        },
-		        {
-		            "colorName":"blue",
-		            "hexValue":"#00f"
-		        },
-		        {
-		            "colorName":"cyan",
-		            "hexValue":"#0ff"
-		        },
-		        {
-		            "colorName":"magenta",
-		            "hexValue":"#f0f"
-		        },
-		        {
-		            "colorName":"yellow",
-		            "hexValue":"#ff0"
-		        },
-		        {
-		            "colorName":"black",
-		            "hexValue":"#000"
-		        }
-		    ]
-		};	
-	//save to local storage
-	var tempColourset = JSON.stringify(Colourset);
-	localStorage.setItem('ColourData', tempColourset);
-
-	}
 
 function DebugText(obj) {
 	var obj_text = obj.colorName;
